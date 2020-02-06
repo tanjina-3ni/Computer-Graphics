2365baaf689299e9838addc8cadd14cfee2f5eeb
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-02-06 12:12:28 +0600

    something went wrong

--- 12_LineClipping.cpp
+++ 12_LineClipping.cpp
@@ -65,46 +65,91 @@ void window(int Xmin, int Ymin, int Xmax, int Ymax, int xm, int ym){
 }
 
 
+int calculateAND(int a1,int b1,int a2,int b2,int a3,int b3,int a4,int b4){
+    int flag=0;
+    if((a1&b1)==0 && (a2&b2)==0 && (a3&b3)==0 && (a4&b4)==0)
+    {
+        cout<<"Candidate"<<endl;
+        flag=1;
+    }
+    else
+    {
+        cout<<"Not Visible"<<endl;
+    }
+    return(flag);
+}
 
-void RegionCode(int X, int Y, int Xmin, int Ymin, int Xmax, int Ymax){
 
-    string b1,b2,b3,b4,code;
-    bool c;
+int RegionCode(int x1, int y1, int x2, int y2, int Xmin, int Ymin, int Xmax, int Ymax){
+
+    int b1,b2,b3,b4,a1,a2,a3,a4,flag;
     int a;
-    a=Y-Ymax;
+    a=y1-Ymax;
     if(a>0)
-        b1='1';
+        b1=1;
     else
-        b1='0';
+        b1=0;
 
-    a=Ymin-Y;
+    a=Ymin-y1;
     if(a>0)
-        b2='1';
+        b2=1;
     else
-        b2='0';
+        b2=0;
 
-    a=X-Xmax;
+    a=x1-Xmax;
     if(a>0)
-        b3='1';
+        b3=1;
     else
-        b3='0';
+        b3=0;
 
-    a=Xmin-X;
+    a=Xmin-x1;
     if(a>0)
-        b4='1';
+        b4=1;
     else
-        b4='0';
+        b4=0;
+       //x2 y2
 
-    code=b1+b2+b3+b4;
-    istringstream("code") >> c; ///TODO:string to boolean
-    cout<<c<<endl;
+    a=y2-Ymax;
+    if(a>0)
+        a1=1;
+    else
+        a1=0;
 
-    return c;
+    a=Ymin-y2;
+    if(a>0)
+        a2=1;
+    else
+        a2=0;
+
+    a=x2-Xmax;
+    if(a>0)
+        a3=1;
+    else
+        a3=0;
+
+    a=Xmin-x2;
+    if(a>0)
+        a4=1;
+    else
+        a4=0;
+
+    //code=b1+b2+b3+b4;
+     //cout<<code<<endl;
+
+    //stringstream geek(code);
+    //geek>>c; ///TODO:string to boolean
+    //cout<<c<<endl;
+
+    if(a1==0 && b1==0 &&a2==0 &&b2==0 &&a3==0 &&b3==0 &&a4==0 &&b4==0 )
+        cout<<"Visible"<<endl;
+    else
+        flag=calculateAND(a1,b1,a2,b2,a3,b3,a4,b4);
+
 
 }
 
 
-void candidate(int x1,int y1, int x2, int y2, int Xmin, int Ymin, int Xmax, int Ymax){
+/*void candidate(int x1,int y1, int x2, int y2, int Xmin, int Ymin, int Xmax, int Ymax){
     bool a,b;
     a = RegionCode( x1, y1,Xmin, Ymin, Xmax, Ymax);
     b = RegionCode( x2, y2,Xmin, Ymin, Xmax, Ymax);
@@ -126,7 +171,7 @@ void candidate(int x1,int y1, int x2, int y2, int Xmin, int Ymin, int Xmax, int 
         }
     }
 
-}
+}*/
 
 
 int main(){
@@ -140,12 +185,17 @@ int main(){
     line(xm,0,xm,2*ym);
 
     int Xmax, Xmin, Ymax, Ymin;
+    int x1,x2,y1,y2;
     cout<<"Enter Xmin Ymin Xmax Ymax"<<endl;
     cin>>Xmin>>Ymin>>Xmax>>Ymax;
 
     window(Xmin, Ymin, Xmax, Ymax, xm, ym);
-    int X=-50,Y=-50;
-    RegionCode( X, Y,Xmin, Ymin, Xmax, Ymax);
+
+//   c3=c1&c2;
+    cout<<"Enter end points of Line please"<<endl<<"x1,y1,x2,y2"<<endl;
+   cin>>x1>>y1>>x2>>y2;
+    BresenhamLine(x1,y1,x2,y2,xm,ym);
+    RegionCode(x1, y1, x2, y2, Xmin, Ymin, Xmax, Ymax);
 
 
     getch();
